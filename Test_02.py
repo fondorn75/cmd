707fb6171c1fc03957708e80c0bcc26edec4e847
Dmitrii Dobrin <zatoichi.cadzicu@gmail.com>
2023-05-21 16:10:25 +0300

    token

--- Test_02.py
+++ Test_02.py
@@ -1,6 +1,12 @@
 import yaml
+import requests
 
 with open("settings.yaml") as f:
     user_set = yaml.safe_load(f)
 
-usertoken = "temp.token"
+response = requests.post(user_set['url'])
+print(response.status_code)
+response.encoding = 'utf-8'
+print(response.json())
+
+# usertoken = "temp.token"
